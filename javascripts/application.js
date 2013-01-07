8752ccfe466fb145aa73e5a4ead147e4889414aa
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2013-01-07 19:38:26 +0100

    Start using percentage

--- javascripts/application.js
+++ javascripts/application.js
@@ -3,6 +3,12 @@ document.addEventListener('touchmove', function (event){
   event.preventDefault();
 });
 
+function setHight () {
+  document.getElementById('board').style.height = window.innerHeight + 'px';
+}
+setHight();
+window.onresize = setHight;
+
 var board = new Board();
 var player = new Player();
 var gameController = new GameController(board, player);
