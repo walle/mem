ea46ccd9c62918475d5ecae4e57c2246f056518a
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2013-01-31 21:13:01 +0100

    Fix release bugs

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -16,7 +16,7 @@ Menu.prototype.playButtonClick = function (event) {
   if (!preferences.playerName) {
     event.preventDefault();
     var game = document.getElementById('game');
-    game.innerHTML = '<div id="player-name-prompt"><input type="text" value="' + t('your_name') + '"/><input type="submit" value="Play" /></div>' + game.innerHTML;
+    game.innerHTML = '<div id="player-name-prompt"><input type="text" value="' + t('your_name') + '"/><input type="submit" value="' + t('play') + '" /></div>' + game.innerHTML;
     var prompt = document.getElementById('player-name-prompt');
     prompt.classList.toggle('show');
     var input = document.getElementsByTagName('input')[0];
